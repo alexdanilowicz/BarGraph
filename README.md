3233555f8d93fb3b73c18f1e7f1d50d30f407d8f
alexdanilowicz <alex.danilowicz@gmail.com>
2021-03-18 23:30:16 -0700

    feat: rewrite script to read from env
    Rewrote the script to read from env. Removed functions that wrote .json files and instead just output to csv once. Added content to README.

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# BarGraph
-Cheers to the Untappd API 🍻
+# BarGraph 🍻
+
+> Get your user data with the Untappd API and run fancy stats on it
+
+## Deploying
+
+The output index.html file is deployed via [surge](https://surge.sh/) to [http://untappd-report.surge.sh/](http://untappd-report.surge.sh/)
+
+## Local Development
+
+We use a simple Python script to hit the Untappd API and R to make graphs and run stats.
+
+### Getting Untappd API Secrets
+
+To run the Python script, you'll need an Untappd `CLIENT_ID` and `CLIENT_SECRET`. Put those in `,`.env` file in root, like so:
+
+```
+echo "CLIENT_ID=123" >> .env
+echo "CLIENT_SECRET=456" >> .env
+```
+
+You can apply for an [Untappd API key](https://untappd.com/api/docs). The Python script will read the `.env` file.
+
+You'll also want to put USERNAMES in your .env as a list, like so:
+
+```
+echo "USERNAMES=bob,alice,carl" >> .env
+```
+
+### Python
+
+#### First time?
+
+Create virutal env:
+
+`python3 -m venv venv`
+
+In the future, we would like to dockerize this.
+
+#### Then:
+
+`source venv/bin/activate`
+
+`pip3 install -r requirements.txt`
+
+Once everything is installed, you can run the script.
+
+```
+python3 main.py --help
+usage: main.py [-h] [--force] [--outfile OUTFILE]
+               [--number-of-unique-beers NUMBER_OF_UNIQUE_BEERS]
+
+Hit the Untappd API for user data 🍻.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --force               Actually make a request. Used so you don't blow
+                        through your Untappd API limit. (default: False)
+  --outfile OUTFILE     Name of outfile. Should match filename in R script.
+                        (default: data.csv)
+  --number-of-unique-beers NUMBER_OF_UNIQUE_BEERS
+                        How many unique beers for each user? (default: 49)
+```
+
+Example usage:
+
+```
+python3 main.py --force --outfile "allBeers.csv" --number-of-unique-beers 49
+```
+
+### R
+
+TODO
---
+++ main.py
@@ -0,0 +1,98 @@
+import csv 
+import requests
+from dotenv import load_dotenv
+import os
+import argparse
+import pandas as pd
+import json 
+
+load_dotenv()
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+USERNAMES = (os.getenv("USERNAMES")).split(",")
+
+URL_ENDPOINT = "https://api.untappd.com/v4/user/beers/"
+UNTAPPD_MAX_LIMT = 50
+DEFAULT_PARAMS = {
+  "limit": str(UNTAPPD_MAX_LIMT),
+  "sort": "highest_rated_you",
+  "client_id": CLIENT_ID,
+  "client_secret": CLIENT_SECRET,
+}
+
+def main():
+  blob_by_user = {} 
+  for user in USERNAMES:
+    blob = {}
+    blob['root'] = []
+    request_user_beers(user, blob)
+    blob_by_user[user] = blob
+  
+  write_blob_by_user_to_one_csv(blob_by_user)
+  print("Done 🍻")
+
+def get_arguments():
+  parser = argparse.ArgumentParser(description='Hit the Untappd API for user data 🍻.',
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+  parser.add_argument("--force", action='store_true', default=False,
+                      help="Actually make a request. Used so you don't blow through your Untappd API limit.")
+
+  parser.add_argument("--outfile", type=str, default="data.csv",
+                      help="Name of outfile. Should match filename in R script.")
+
+  # TODO: this wrongly assumes all users have checked in roughly the same number of beers
+  parser.add_argument("--number-of-unique-beers", type=int, default=49,
+                      help="How many unique beers for each user?")
+
+  return parser.parse_args()
+
+def request_user_beers(user, blob):
+    url = URL_ENDPOINT + user
+
+    for offset in range(0, args.number_of_unique_beers, UNTAPPD_MAX_LIMT):
+      params = DEFAULT_PARAMS
+      if offset > 1:
+        params = {**DEFAULT_PARAMS, 'offset': str(o)}
+      
+      if args.force: 
+        r = requests.get(url, params = params)
+        response = r.json()
+      else: 
+        response = { 
+          'response': 
+            {'beers': 
+              {'count': 1, 
+              'items': [
+                {
+                  user: "test-response"
+                }
+              ]
+            }
+          }
+        }
+
+      print(response)
+      print("Fetched response with offset: " + str(offset) + " 🍺...")
+      construct_user_json(blob, response)
+
+def construct_user_json(blob, response):
+  for beer in response['response']['beers']['items']:
+    blob['root'].append(beer)
+
+def write_blob_by_user_to_one_csv(blob_by_user):
+  data = pd.concat([read_blob_into_df(blob_by_user[user], user) for user in USERNAMES])
+  data.to_csv(args.outfile)
+
+def read_blob_into_df(blob, user):
+  # flatten data and read into frame
+  df = pd.json_normalize(blob, record_path =["root"])
+  # track which user it came from
+  df.insert(0, "user", user)
+  return df
+
+if __name__ == "__main__":
+  args = get_arguments()
+  main()
+
+
